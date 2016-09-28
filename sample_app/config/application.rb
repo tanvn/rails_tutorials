1dc5fad6eb61d73619073ed92a5eb34d9329039f
Vu Nhat Tan <tan.vu@mobitech.co.jp>
2016-09-28 18:12:09 +0900

    add file chapter 6

--- sample_app/config/application.rb
+++ sample_app/config/application.rb
@@ -2,12 +2,15 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# タイムゾーンを東京(JST +9:00)に設定
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    p config
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
